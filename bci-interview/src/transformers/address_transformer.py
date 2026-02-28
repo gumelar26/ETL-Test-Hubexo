19152f7c33f917709b90e2fd148cf9a59f1449b3
gumelar26 <gumelarmuhamad26@gmail.com>
2026-02-28 10:52:58 +0700

    add address_transformer

--- bci-interview/src/transformers/address_transformer.py
+++ bci-interview/src/transformers/address_transformer.py
@@ -1,6 +1,18 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 def transform(address_iter):
     """
     Transforms an iterator of address dictionaries by enriching each address.
     Yields enriched addresses one by one.
     """
-    pass
+    token = os.getenv("LOCATIONIQ_API_KEY")
+
+    if not token:
+        raise ValueError("LOCATIONIQ_API_KEY not found in environment variables")
+    
+    enrich_address = get_structured_address(token, address_iter)
+    
+    yield enrich_adress
